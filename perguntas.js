dbb1ab173ed9fd7dd907c8b94158b608b34efeda
Nicolibubna <bubna.nicoli@escola.pr.gov.br>
2025-06-12 12:12:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,68 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da suiça?',
+    'A capital da suiça é berna '
+)
+
+criaCartao(
+    'historia',
+    'Quais são as 3 principais causas da Segunda Guerra Mundial??',
+    'o revanchismo alemão, a expansão nazifascista e a invasão da Polônia.'
+)
+  
+criaCartao(
+    'matematica',
+    'Há um pato entre dois patos, um pato atrás de um pato e um pato na frente de outro pato. De quantos patos estamos falando?',
+    '3 patos'
+)
+
+criaCartao(
+    'biologia',
+    'Os vírus são organismos obrigatórios?',
+    'Os vírus são parasitas intracelulares obrigatórios, sendo considerados seres vivos por alguns autores e não vivos por outros'
+)
+
+criaCartao(
+    'portugues',
+    'qual o verbo do futuro de estar',
+    'estarei'
+)
+
+criaCartao(
+    'sociologia',
+    'O que é relativismo em sociologia?',
+    'é um conceito e perspectiva antropológica que se opõe à categorização de culturas como “superior” ou “inferior'
+)
+criaCartao(
+    'projeto de vida',
+    'o que é mercado digital',
+    'todas as atividades comerciais que acontecem online, abrangendo a compra e venda de produtos e serviços'
+
+)
+
+criaCartao(
+    'ed financeira',
+    'qual e a regra do 50,30,20',
+    '50% para necessidades,30% para desejos,20% para poupança'
+
+)
+criaCartao(
+    'mat 1',
+    'qual a importancia dela',
+    'resolver calculos de logica'
+
+)
+vcriaCartao(
+    'matematica 2',
+    'para oque serve',
+    'refere-se a um curso ou módulo de matemática de nível secundário, que normalmente aborda tópicos como álgebra, geometria, trigonometria e probabilidade'
+)
+
+
+
